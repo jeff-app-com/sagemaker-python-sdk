8884ddb50a9bb6286fda032694ffad07781468e3
Alvils Sture <alvilssture@Alvilss-MacBook-Pro.local>
2025-07-17 10:39:12 +0300

    added network

--- src/sagemaker/serve/model_server/torchserve/server.py
+++ src/sagemaker/serve/model_server/torchserve/server.py
@@ -30,6 +30,7 @@ class LocalTorchServe:
             "serve",
             detach=True,
             auto_remove=True,
+            network="sagemaker",
             network_mode="sagemaker",
             volumes={
                 Path(model_path): {
